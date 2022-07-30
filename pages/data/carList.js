c71328b2557fbaa70099334bdcabf75cf0d659b9
Tannistha Biswas <tannisthabiswasofficial@gmail.com>
2022-07-30 14:13:13 +0530

    initial

---
+++ pages/data/carList.js
@@ -0,0 +1,27 @@
+export const carList = [
+  {
+    imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+    service: 'UberGo',
+    multiplier: 1,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/YDYMKny/uberxl.png',
+    service: 'Go Sedan',
+    multiplier: 1.5,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/Xx4G91m/uberblack.png',
+    service: 'Premier',
+    multiplier: 2,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+    service: 'UberXL',
+    multiplier: 1.2,
+  },
+  {
+    imgUrl: ' https://i.ibb.co/1nStPWT/uberblacksuv.png',
+    service: 'Uber Rentals',
+    multiplier: 2.8,
+  }
+]
